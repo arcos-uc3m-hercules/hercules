805435aecc571b1a93df30ac297c5a8b0639bfbb
Genaro Sanchez <gesanche@pa.uc3m.es>
2025-01-29 14:03:11 +0100

    Add improvements to the deployment script, temporal files are move to a temporal directory.

--- scripts/check-servers.sh
+++ scripts/check-servers.sh
@@ -3,12 +3,17 @@
 SERVER_TYPE=$1
 SERVER_NUMBER=$2
 ACTION=$3 # expected string action, e.g., down when servers are stopped.
-ATTEMPS=10
+HERCULES_PATH=$4
+ATTEMPS=100
 i=1
 
-echo "-- Hercules path: ${HERCULES_PATH}"
+## To check if the temporal directory exists.
+if [ ! -d "${HERCULES_PATH}/tmp" ]; then
+    echo "[ERROR] Temporal path ${HERCULES_PATH}/tmp does not exist. Please, create it, or set the "HERCULES_PATH" option in the configuration file to overwrite it."
+    exit 1
+fi
 
-FILE="./tmp/$SERVER_TYPE-hercules-$SERVER_NUMBER-$ACTION"
+FILE="${HERCULES_PATH}/tmp/$SERVER_TYPE-hercules-$SERVER_NUMBER-$ACTION"
 ## Checks if the file exists.
 until [ -f $FILE ]; do
     echo "Waiting for $FILE, attemp $i"
