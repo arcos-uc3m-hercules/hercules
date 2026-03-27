3421a15b9177662474abdbeaa10a72eea4ec2380
Genaro Sanchez <gesanche@pa.uc3m.es>
2026-03-27 17:31:26 +0100

    Stop tracking bash scripts directory.

--- bash/tests/metadata/move-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-HERCULES_DEBUG_LEVEL=none HERCULES_CONF=/beegfs/home/javier.garciablas/hercules/bash/tests/metadata/configurations_hercules/hercules.conf LD_PRELOAD=/beegfs/home/javier.garciablas/hercules/build/tools/libhercules_posix.so mkdir /mnt/hercules/dir
-HERCULES_DEBUG_LEVEL=none HERCULES_CONF=/beegfs/home/javier.garciablas/hercules/bash/tests/metadata/configurations_hercules/hercules.conf LD_PRELOAD=/beegfs/home/javier.garciablas/hercules/build/tools/libhercules_posix.so touch /mnt/hercules/dir/myfile.txt
-HERCULES_DEBUG_LEVEL=all HERCULES_CONF=/beegfs/home/javier.garciablas/hercules/bash/tests/metadata/configurations_hercules/hercules.conf LD_PRELOAD=/beegfs/home/javier.garciablas/hercules/build/tools/libhercules_posix.so mv /mnt/hercules/dir/myfile.txt /mnt/hercules/dir
-
